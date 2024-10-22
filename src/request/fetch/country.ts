77d4bbddc8f472bf069934a61035b3f8bd115497
Sourav Bapari <codexsourav404@gmail.com>
2024-10-22 23:59:13 +0530

    update

--- src/request/fetch/country.ts
+++ src/request/fetch/country.ts
@@ -14,22 +14,25 @@ export interface Country {
   country: string;
   cities: string[];
 }
-
 export const getCountryCity = async () => {
-  let req: Country[] = [];
+  let req: CountryList = {
+    error: false,
+    msg: "",
+    data: [],
+  };
   if (typeof window !== "undefined" && window.localStorage) {
     const data = window.localStorage.getItem("CountryList");
     if (data) {
       req = JSON.parse(data);
     }
   }
-  if (req.length == 0) {
-    req = await _countryCityApi.get("api/v0.1/countries").send<Country[]>();
+  if (req.data.length == 0) {
+    req = await _countryCityApi.get("api/v0.1/countries").send<CountryList>();
     console.log(req);
 
     if (typeof window !== "undefined" && window.localStorage) {
       window.localStorage.setItem("CountryList", JSON.stringify(req));
     }
   }
-  return req;
+  return req.data;
 };
